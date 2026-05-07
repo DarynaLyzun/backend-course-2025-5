af406384eddf050211fe23683bca0183a3ff8051
Дарина Лизун bc2024-5 <daryna.lyzun@lnu.edu.ua>
2026-05-07 12:57:16 +0300

    Added image routing based on URL path

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ const path = require('path');
 const http = require('http');
 
 program
-    .requiredOption("-h, --host <address>")
-    .requiredOption("-p, --port <number>")
-    .requiredOption("-c, --cache <directory>");
+  .requiredOption("-h, --host <address>")
+  .requiredOption("-p, --port <number>")
+  .requiredOption("-c, --cache <directory>");
 
 program.parse();
 const options = program.opts();
@@ -15,13 +15,35 @@ const cachePath = path.join(__dirname, options.cache);
 
 if (!fs.existsSync(cachePath)) 
 {
-    fs.mkdirSync(cachePath, { recursive: true });
+  fs.mkdirSync(cachePath, { recursive: true });
 }
 
-const server = http.createServer((req, res) => 
+const server = http.createServer(async (req, res) => 
 {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Server is running\n');
+  const baseURL = `http://${options.host}:${options.port}`;
+  const parsedUrl = new URL(req.url, baseURL);
+  const code = parsedUrl.pathname.slice(1);
+
+  const filePath = path.join(options.cache, `${code}.jpg`);
+
+  if (!/^\d{3}$/.test(code)) 
+  {
+    res.writeHead(400, { 'Content-Type': 'text/plain; charset=utf-8' });
+    res.end("Bad Request: Вкажіть валідний HTTP код у шляху");
+    return;
+  }
+
+  try 
+  {
+    const data = await fs.promises.readFile(filePath);
+    res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+    res.end(data);
+  } 
+  catch (error) 
+  {
+    res.writeHead(404, { 'Content-Type': 'text/plain; charset=utf-8' });
+    res.end("Not Found: Картинку для цього коду не знайдено в локальному кеші");
+  }
 });
 
 server.listen(options.port, options.host, () => 
